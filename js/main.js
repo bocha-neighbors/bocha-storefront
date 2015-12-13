c69feda8e38995e4d245b02508e562f5fd182a7a
Zachary Smith <billyzacsmith@gmail.com>
2015-12-12 17:50:14 -0700

    When you click the Add button, added items go into a cart array, which represents the order on the client side. The cart display is refreshed whenever the add button is clicked, showing the current order.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(function(){
   var url = 'http://localhost:8080/catalog'
+  var cart = []
 
   $.get(url)
   .done(function(data) {
+
     listCatalog(data)
     highlightFirstItem()
     highlightSelectedItem()
-    handleAddItem(data)
+    handleAddItem(data, cart)
   })
 
 })
 
 function listCatalog(data) {
-  console.log(data)
   for (var i=0; i < data.length; i++) {
     var item =
     '<div class="item"' +
@@ -38,11 +39,39 @@ function highlightSelectedItem() {
   })
 }
 
-function handleAddItem(data) {
+function handleAddItem(data, cart) {
   $('.add').click(function(event) {
     event.preventDefault()
     var selectedIndex = $('.selected').attr('data-id') - 1
     var item = data[selectedIndex]
-    console.log(item.name)
+    console.log('you clicked. heres one item', item)
+    addToCart(item, cart)
   })
+}
+
+function addToCart(item, cart) {
+  // Add to cart array
+  cart.push(item)
+  console.log('added item to cart. heres the cart', cart)
+  // Refresh display of cart
+  displayCart(cart)
+}
+
+function displayCart(cart) {
+  // Clear any existing items in the displayed cart
+  $('.cart').text('')
+
+  // Loop over the cart, adding each item to the cart div
+  for (var i=0; i < cart.length; i++) {
+    var item =
+    '<div class="item"' +
+    'data-id="' + cart[i].id + '">' +
+    cart[i].name +
+    '<span class="price">' +
+    cart[i].price +
+    '</span>' +
+    '</div>'
+
+    $('.cart').append(item)
+  }
 }
